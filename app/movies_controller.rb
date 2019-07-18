7810564624904b404fafc8a425dc016201db89ea
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-18 00:20:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,9 @@ end
 
 def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
   Movie.create do |m|
-    { title: "Home Alone", release_date: 1990 }
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
